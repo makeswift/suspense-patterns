84faf1bc4aa84c8e753c8e9a2cf1933c0880e9fe
Miguel Oller <maoa22@gmail.com>
2024-10-30 02:19:57 -0400

    feat: update error example to throw in RSC

--- app/error-boundary/page.tsx
+++ app/error-boundary/page.tsx
@@ -1,13 +1,18 @@
+import { Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
 export default function Page() {
   return (
     <ErrorBoundary fallback={<div>An error has occurred!</div>}>
-      <Boom />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Boom />
+      </Suspense>
     </ErrorBoundary>
   )
 }
 
-function Boom(): JSX.Element {
+async function Boom(): Promise<JSX.Element> {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
   throw new Error('Boom!')
 }
--- app/error-file/page.tsx
+++ app/error-file/page.tsx
@@ -1,3 +1,5 @@
-export default function Page() {
+export default async function Page() {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
   throw new Error('Boom!')
 }
